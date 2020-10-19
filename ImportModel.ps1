ddb698876aa5108b9b652f56319169f8081e9872
Jose Marques Feijão Junior <35135021+Marquesfeijao@users.noreply.github.com>
2020-10-19 22:03:48 +0200

    Update ImportModel.ps1
    Finished the first step

--- ImportModel.ps1
+++ ImportModel.ps1
@@ -1,18 +1,28 @@
-$ActionFGColor      = "White"
-$ActionBGColor      = "DarkGreen"
-$FinishedFGColor    = "Green"
+#region Assemblies
+Add-Type -AssemblyName System
+Add-Type -AssemblyName System.IO
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName PresentationFramework
+#endregion
+
+#region Variables
+$ActionFGColor = "White"
+$ActionBGColor = "DarkGreen"
+$FinishedFGColor = "Green"
 $ExecutionStartTime = $(Get-Date)
 
 $TaskStartTime = $(Get-Date)
 
-$PackagesLocalDirectory = 'C:\AOSService\PackagesLocalDirectory'
-$ISVModelToImport       = 'C:\temp\Model'
+$ISVModelToImport = 'C:\temp\Model'
+#endregion
+
 
 #region Methods
-function StartImport($StartProcess){
+function StartImport($StartProcess) {
     $ElapsedTimeFGColor = "Cyan"
-    Write-Host "****** Starting Process ******" -ForegroundColor $ElapsedTimeFGColor
+    Write-Host "****** Starting Process ******" -ForegroundColor $ElapsedTimeFGColor    
 }
+
 function ElapsedTime($TaskStartTime) {
     $ElapsedTimeFGColor = "Cyan"
     $ElapsedTime = New-TimeSpan $TaskStartTime $(Get-Date)
@@ -36,17 +46,14 @@ function Pause ($message) {
     }
 }
 
-function StartServices(){
+function StartServices() {
     Write-Host ""
     Write-Host "*** Starting services... ***" -ForegroundColor $ActionFGColor -BackgroundColor $ActionBGColor
     Write-Host ""
 
     $TaskStartTime = $(Get-Date)
 
-    Get-Service DynamicsAxBatch `
-        , Microsoft.Dynamics.AX.Framework.Tools.DMF.SSISHelperService.exe `
-        , W3SVC `
-        , MR2012ProcessService `
+    Get-Service W3SVC `
         , aspnet_state `
     | Start-Service
 
@@ -55,35 +62,121 @@ function StartServices(){
     iisreset.exe
 }
 
-function StopServices(){
+function StopServices() {
     Write-Host ""
     Write-Host "*** Stopping services... ***" -ForegroundColor $ActionFGColor -BackgroundColor $ActionBGColor
     Write-Host ""
 
     $TaskStartTime = $(Get-Date)
 
-    Get-Service DynamicsAxBatch `
-        , Microsoft.Dynamics.AX.Framework.Tools.DMF.SSISHelperService.exe `
-        , W3SVC `
-        , MR2012ProcessService `
+    Get-Service W3SVC `
         , aspnet_state `
     | Stop-Service -Force
 
     Finished $TaskStartTime
 }
+
+function CheckServices($ServicesStatus) {
+
+    Get-Service W3SVC `
+        , aspnet_state `
+    | Where-Object {
+        switch ($ServicesStatus) {
+            "Running" { 
+                if ($_.Status -eq "Running") {
+                    StopServices
+                }
+            }
+            "Stopped" { 
+                if ($_.Status -eq "Stopped") {
+                    StartServices
+                }
+            }
+        }
+    }
+}
+
+function OpenFileDialog () {
+
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+        InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+        Filter           = 'Models (*.axmodel)| *.axmodel'
+        Multiselect      = 1
+    }
+
+    $FileBrowser.ShowDialog()
+    
+    if (![String]::IsNullOrEmpty($FileBrowser.FileName)) {
+        ForEach ($fileName in $FileBrowser.FileNames) {
+            Copy-Item $fileName -Destination $ISVModelToImport
+        }
+    }
+
+    return $FileBrowser.SafeFileNames
+}
+
+function Import-DAXModel($ImportFileNames) {
+    $ReturnImport = ""
+    ForEach ($FileName in $ImportFileNames) {
+        if ($FileName -ne "Ok") {
+            $ReturnImport += Invoke-ImportModelUtil $FileName
+        }
+    }
+
+    return $ReturnImport
+}
+
+function Invoke-ImportModelUtil($ModelFileName) {
+
+    $ModelStore     = "$($env:SystemDrive)\AOSService\PackagesLocalDirectory"
+    $BinPath        = Join-Path -Path $ModelStore -ChildPath "Bin"
+    $ModelUtil      = Join-Path -Path $BinPath -ChildPath "ModelUtil.exe"    
+    $ModelToimport  = Join-Path -Path $ISVModelToImport -ChildPath $ModelFileName
+
+    $ReplaceArgs = @("-replace";
+        "-MetadataStorePath=`"$ModelStore`"";
+        "-file=`"$ModelToimport`"";
+        "-force"
+        ) 
+    & $ModelUtil $ReplaceArgs
+}
 #endregion
 
 StartImport $(Get-Date)
 
-StopServices 
+StopServices
 
-Pause("Press any key to start the services...")
+$ImportFileNames = OpenFileDialog
+
+if ($ImportFileNames[0] -eq "Ok") {
+    $ReturnModels = Import-DAXModel $ImportFileNames 
+    
+    foreach ($modelImported in $ReturnModels) {
+        Write-Host $modelImported
+    }
+}
 
 StartServices
 
-Write-Host ""
+
+# $messageBox = [System.Windows.MessageBox]::Show('Do you want to stop the services?','Stop the services','YesNoCancel',[System.Windows.MessageBoxImage]::Exclamation)
+
+# if ($messageBox -eq [System.Windows.MessageBoxResult]::Yes)
+# {
+#     [System.Windows.MessageBox]::Show('Services stopped', 'Notication', 'OK', 'Info');
+# }
+# else {
+#     [System.Windows.MessageBox]::Show('No!!!');
+# }
+# StopServices 
+
+# Pause("Press any key to start the services...")
+
+# StartServices
+
+Write-Host "*****************" -ForegroundColor $ActionFGColor -BackgroundColor $ActionBGColor
 Write-Host "*** All done! ***" -ForegroundColor $ActionFGColor -BackgroundColor $ActionBGColor
-Write-Host ""
+Write-Host "*****************" -ForegroundColor $ActionFGColor -BackgroundColor $ActionBGColor
 
 ElapsedTime $ExecutionStartTime
 
